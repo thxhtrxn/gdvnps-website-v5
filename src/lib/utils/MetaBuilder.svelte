14dae66d351e56e7ac8bd35fe151089a8adfb07e
tranxuanthanh-sk <tranxuanthanhreal@gmail.com>
2025-01-28 20:02:34 +0700

    feat: init

---
+++ src/lib/utils/MetaBuilder.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let title: string;
+	export let desc: string;
+
+	let urlLocation: string;
+	onMount(() => (urlLocation = window.location.pathname));
+</script>
+
+<svelte:head>
+	<title>{title} | GDVNPS</title>
+	<meta name="description" content={`${desc}`} />
+
+	<meta property="og:title" content={`${title}`} />
+	<meta property="og:description" content={`${desc}`} />
+	<meta property="og:url" content={`https://gdvnps.site${urlLocation}`} />
+
+	<meta name="twitter:title" content={`${title}`} />
+	<meta name="twitter:description" content={`${desc}`} />
+</svelte:head>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import MetaBuilder from '$lib/utils/MetaBuilder.svelte';
+</script>
+
+<MetaBuilder
+	title="Trang chủ"
+	desc="GDVNPS là một dự án Geometry Dash Private Server (GDPS) dành cho cộng đồng người chơi Geometry Dash tại Việt Nam"
+/>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
