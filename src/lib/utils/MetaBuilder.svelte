0bdc8e80dc2a4abf0a610ed785d5b7b087a9fe55
tranxuanthanh-sk <tranxuanthanhreal@gmail.com>
2025-02-13 20:31:11 +0700

    fix: tiny change

--- src/lib/utils/MetaBuilder.svelte
+++ src/lib/utils/MetaBuilder.svelte
@@ -15,7 +15,7 @@
 
 	<meta property="og:title" content={`${title}`} />
 	<meta property="og:description" content={`${desc}`} />
-	<meta property="og:url" content={`${page.url.hostname}/${page.url.pathname}`} />
+	<meta property="og:url" content={`${page.url.hostname}${page.url.pathname}`} />
 
 	<meta name="twitter:title" content={`${title}`} />
 	<meta name="twitter:description" content={`${desc}`} />
