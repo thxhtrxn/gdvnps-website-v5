771496f20449dde197a1a6e6d00f2375f2c89f6c
vradient <167614599+TacoEnjoyer@users.noreply.github.com>
2025-02-19 22:45:55 +0700

    feat: add privacy policy and terms and conditions content

--- src/routes/privacy-policy/+page.svelte
+++ src/routes/privacy-policy/+page.svelte
@@ -1,1 +1,26 @@
-/
+<script lang="ts">
+	import MetaBuilder from '$lib/utils/MetaBuilder.svelte';
+</script>
+
+<MetaBuilder
+	title="GDVNPS - Điều khoản và điều kiện"
+	desc="GDVNPS là một dự án Geometry Dash Private Server (GDPS) dành cho cộng đồng người chơi Geometry Dash tại Việt Nam"
+/>
+
+<!--content-->
+
+<div class="pt-16">
+	<h1 class="text-4xl font-bold">Thông tin chúng tôi thu thập</h1>
+	<li class="pt-12 text-base">
+		Chúng tôi có thể thu thập thông tin cá nhân như tên, địa chỉ email và bất kỳ thông tin nào khác
+		mà bạn tự nguyện cung cấp khi đăng ký nhận bản tin, tham gia thảo luận hoặc liên hệ với chúng
+		tôi qua trang web của chúng tôi.
+	</li>
+	<h1 class="pt-12 text-4xl font-bold">Chúng tôi sử dụng thông tin của bạn như thế nào</h1>
+	<li class="pt-12 text-base">
+		Thông tin chúng tôi thu thập được sử dụng để cải thiện trang web của chúng tôi, trả lời các câu
+		hỏi của bạn, gửi cho bạn các bản cập nhật và cung cấp trải nghiệm được cá nhân hóa. Chúng tôi
+		không chia sẻ thông tin của bạn với bên thứ ba trừ khi cần thiết để cung cấp dịch vụ của chúng
+		tôi hoặc theo yêu cầu của pháp luật.
+	</li>
+</div>
--- src/routes/terms-and-conditions/+page.svelte
+++ src/routes/terms-and-conditions/+page.svelte
@@ -1,1 +1,51 @@
-/
+<script lang="ts">
+	import MetaBuilder from '$lib/utils/MetaBuilder.svelte';
+</script>
+
+<MetaBuilder
+	title="GDVNPS - Điều khoản và điều kiện"
+	desc="GDVNPS là một dự án Geometry Dash Private Server (GDPS) dành cho cộng đồng người chơi Geometry Dash tại Việt Nam"
+/>
+
+<!--content-->
+
+<div class="pt-16">
+	<h1 class="text-4xl font-bold">Điều khoản và điều kiện</h1>
+	<li class="pt-12 text-base">
+		Chào mừng đến với GDVNPS. Bằng cách truy cập trang web của chúng tôi, bạn đồng ý tuân thủ và bị
+		ràng buộc bởi các điều khoản và điều kiện sau. Vui lòng xem xét kỹ các điều khoản này. Nếu bạn
+		không đồng ý với các điều khoản này, bạn không nên tiếp tục truy cập trang web.
+	</li>
+	<h1 class="pt-12 text-4xl font-bold">Truy cập và sử dụng trang web</h1>
+	<li class="pt-12 text-base">
+		Bạn đồng ý sử dụng trang web này theo tất cả các luật và quy định hiện hành. Bạn không được sử
+		dụng trang web của chúng tôi theo bất kỳ cách nào có thể gây hại, phá hủy hoặc can thiệp vào
+		hoạt động của trang web.
+	</li>
+	<h1 class="pt-12 text-4xl font-bold">Sử dụng thông tin của bạn</h1>
+	<li class="pt-12 text-base">
+		Thông tin bạn cung cấp sẽ được sử dụng để nâng cao trải nghiệm của bạn trên trang web của chúng
+		tôi, trả lời các câu hỏi và gửi cho bạn các bản cập nhật. Chúng tôi không chia sẻ thông tin của
+		bạn với bên thứ ba trừ khi cần thiết để cung cấp dịch vụ của chúng tôi hoặc theo yêu cầu của
+		pháp luật.
+	</li>
+	<h1 class="pt-12 text-4xl font-bold">Liên kết của bên thứ ba</h1>
+	<li class="pt-12 text-base">
+		Trang web của chúng tôi có thể chứa các liên kết đến các trang web của bên thứ ba. Chúng tôi
+		không chịu trách nhiệm về nội dung hoặc chính sách bảo mật của các trang web bên ngoài này. Bạn
+		có trách nhiệm xem xét các điều khoản và chính sách bảo mật của họ trước khi tương tác với họ.
+	</li>
+	<h1 class="pt-12 text-4xl font-bold">Giới hạn trách nhiệm</h1>
+	<li class="pt-12 text-base">
+		Chúng tôi cố gắng cung cấp thông tin chính xác và cập nhật trên trang web của mình. Tuy nhiên,
+		chúng tôi không bảo đảm về tính chính xác hoặc đầy đủ của bất kỳ nội dung nào trên trang web.
+		Trong mọi trường hợp, GDVNPS sẽ không chịu trách nhiệm cho bất kỳ thiệt hại nào phát sinh từ
+		việc bạn sử dụng trang web.
+	</li>
+	<h1 class="pt-12 text-4xl font-bold">Thay đổi Điều khoản</h1>
+	<li class="pt-12 text-base">
+		Chúng tôi có quyền sửa đổi các điều khoản và điều kiện này bất kỳ lúc nào. Các thay đổi sẽ có
+		hiệu lực ngay khi được đăng lên trang web. Việc bạn tiếp tục truy cập trang web này có nghĩa là
+		bạn chấp nhận bất kỳ điều khoản đã sửa đổi nào.
+	</li>
+</div>
