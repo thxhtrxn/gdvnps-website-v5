f4597ffd287dab01f52428a1775c779f672d65e1
tranxuanthanh-sk <tranxuanthanhreal@gmail.com>
2025-02-23 10:23:52 +0700

    fix(SEO): patch

--- src/lib/utils/MetaBuilder.svelte
+++ src/lib/utils/MetaBuilder.svelte
@@ -12,10 +12,11 @@
 <svelte:head>
 	<title>{title}</title>
 	<meta name="description" content={`${desc}`} />
+	<link rel="canonical" href={`https://gdvnps.site${page.url.pathname}`} />
 
 	<meta property="og:title" content={`${title}`} />
 	<meta property="og:description" content={`${desc}`} />
-	<meta property="og:url" content={`${page.url.hostname}${page.url.pathname}`} />
+	<meta property="og:url" content={`https://gdvnps.site${page.url.pathname}`} />
 
 	<meta name="twitter:title" content={`${title}`} />
 	<meta name="twitter:description" content={`${desc}`} />
