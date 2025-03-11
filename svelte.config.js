0f6e0703b9c6d18f8ab37d9492b4803abf5bbce6
tranxuanthanh-sk <tranxuanthanhreal@gmail.com>
2025-03-11 17:46:19 +0700

    feat: add something

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,6 +13,14 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
+		// adapter: adapter({
+		// 	pages: 'build',
+		// 	assets: 'build',
+		// 	fallback: undefined,
+		// 	precompress: false,
+		// 	strict: true,
+		// 	trailingSlash: 'always'
+		// })
 	}
 };
 
