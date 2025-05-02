63a7f0b387a41f3446f8bb07b8eb9afddc52fcdc
thxhtrxn <tranxuanthanhreal@gmail.com>
2025-05-02 16:56:06 +0700

    bun

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 // import adapter from '@sveltejs/adapter-vercel';
-// import adapter from 'svelte-adapter-bun';
+import adapter from 'svelte-adapter-bun';
 // import adapter from '@sveltejs/adapter-node';
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,15 +14,15 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		// adapter: adapter(),
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true,
-			trailingSlash: 'always'
-		}),
+		adapter: adapter(),
+		// adapter: adapter({
+		// 	pages: 'build',
+		// 	assets: 'build',
+		// 	fallback: undefined,
+		// 	precompress: false,
+		// 	strict: true,
+		// 	trailingSlash: 'always'
+		// }),
 		alias: {
 			$routes: 'src/routes/'
 		},
