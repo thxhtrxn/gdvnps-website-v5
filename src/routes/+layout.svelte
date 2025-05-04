291655d199f038f2dac61d443fdc97ecb8c78875
vradient <167614599+TacoEnjoyer@users.noreply.github.com>
2025-05-04 13:50:46 +0700

    ?kjdsfkdfdjdsfkjdfkjdshfhdsfkdsfjskljkljfdskjfkdsjfkljkldfjfkdsjfklsjdfkls

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,8 @@
 	import { QueryClientProvider, QueryClient } from '@tanstack/svelte-query';
 	const queryClient = new QueryClient();
 
+	import './app.css';
+
 	import Navbar from '$lib/components/Navbar/index.svelte';
 	import Footer from '$lib/components/Footer/index.svelte';
 	let { children } = $props();
